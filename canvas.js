710228599282f78f2802479306caf3193366ff89
Rachael Momber <rlmomber@gmail.com>
2023-12-24 11:53:02 +0900

    auto creating of seats in one row successful

--- canvas.js
+++ canvas.js
@@ -7,16 +7,16 @@ canvas.height = window.innerHeight;
 // gets us access to all functions, etc
 var ctx = canvas.getContext("2d");
 
-//customize shapes
-ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
-// (x, y, width, height)
-ctx.fillRect(300, 234, 89, 89);
-ctx.fillStyle = "rgba(150, 100, 50, 0.5)";
-ctx.fillRect(32, 90, 80, 100);
-ctx.fillStyle = "rgba(200, 100, 200, 0.5)";
-ctx.fillRect(90, 321, 100, 100);
-ctx.fillStyle = "rgba(0, 50, 50, 0.5)";
-ctx.fillRect(300, 3, 99, 70);
+// //customize shapes
+// ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+// // (x, y, width, height)
+// ctx.fillRect(300, 234, 89, 89);
+// ctx.fillStyle = "rgba(150, 100, 50, 0.5)";
+// ctx.fillRect(32, 90, 80, 100);
+// ctx.fillStyle = "rgba(200, 100, 200, 0.5)";
+// ctx.fillRect(90, 321, 100, 100);
+// ctx.fillStyle = "rgba(0, 50, 50, 0.5)";
+// ctx.fillRect(300, 3, 99, 70);
 
 // Lines
 ctx.beginPath();
@@ -47,4 +47,20 @@ for (var i = 0; i < 10; i++) {
   ctx.arc(x, y, 30, 0, Math.PI * 2, false);
   ctx.strokeStyle = "rgba(" + Math.random() * 251 + ", " + Math.random() * 251 + ", " + Math.random() * 251 + ", 0.5)";
   ctx.stroke();
+}
+
+var max_seats = 25
+var columns = 5
+var rows = 5
+var working_width = window.innerWidth / (columns * 2 + 1)
+var working_height = window.innerHeight / (rows * 2 + 1)
+var x_coor = working_width
+var y_coor = working_height
+for (var i = 0; i < columns; i++) {
+  for (var i = 0; i < rows; i++) {
+    ctx.fillStyle = "black";
+    ctx.fillRect(x_coor, y_coor, working_width, working_height);
+    x_coor += working_width * 2;
+  }
+  y_coor += working_height * 2;
 }
