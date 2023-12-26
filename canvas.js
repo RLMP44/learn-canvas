1bbf9a771aeb0ba17b116da039832fc17b778aa3
Rachael Momber <rlmomber@gmail.com>
2023-12-26 11:35:00 +0900

    working on making auto generate with various rows and columns

--- canvas.js
+++ canvas.js
@@ -18,17 +18,17 @@ var ctx = canvas.getContext("2d");
 // ctx.fillStyle = "rgba(0, 50, 50, 0.5)";
 // ctx.fillRect(300, 3, 99, 70);
 
-// Lines
-ctx.beginPath();
-// starting point
-ctx.moveTo(50, 300);
-// state where we want the line to go to
-ctx.lineTo(300, 100);
-ctx.lineTo(400,300);
-// customize line
-ctx.strokeStyle = "#fa34a3";
-// draw the line
-ctx.stroke();
+// // Lines
+// ctx.beginPath();
+// // starting point
+// ctx.moveTo(50, 300);
+// // state where we want the line to go to
+// ctx.lineTo(300, 100);
+// ctx.lineTo(400,300);
+// // customize line
+// ctx.strokeStyle = "#fa34a3";
+// // draw the line
+// ctx.stroke();
 
 // Arc / Circle
 // ctx.beginPath();
@@ -38,39 +38,60 @@ ctx.stroke();
 
 // creating multiple circles
 // set i variable, increment i every time it is run
-for (var i = 0; i < 10; i++) {
-  // returns a number anywhere from 0 - 1
-  var x = Math.random() * window.innerWidth;
-  var y = Math.random() * window.innerHeight;
+// for (var i = 0; i < 10; i++) {
+//   // returns a number anywhere from 0 - 1
+//   var x = Math.random() * window.innerWidth;
+//   var y = Math.random() * window.innerHeight;
 
-  ctx.beginPath();
-  ctx.arc(x, y, 30, 0, Math.PI * 2, false);
-  ctx.strokeStyle = "rgba(" + Math.random() * 251 + ", " + Math.random() * 251 + ", " + Math.random() * 251 + ", 0.5)";
-  ctx.stroke();
-}
+//   ctx.beginPath();
+//   ctx.arc(x, y, 30, 0, Math.PI * 2, false);
+//   ctx.strokeStyle = "rgba(" + Math.random() * 251 + ", " + Math.random() * 251 + ", " + Math.random() * 251 + ", 0.5)";
+//   ctx.stroke();
+// }
+
+
 
 var max_seats = 25;
-var columns = 5;
+var columns = 3;
 var rows = 5;
 var working_width = window.innerWidth / (columns * 2 + 1);
 var working_height = window.innerHeight / (rows * 2 + 1);
 var x_coor = working_width;
 var y_coor = working_height;
 
-function generateDesks(rows, columns, x_coor, y_coor, working_width, working_height) {
-  for (var c = 0; c < columns; c++) {
-    generateRow(rows, x_coor, y_coor, working_width, working_height);
-    y_coor += working_height * 2;
+function generateDesks() {
+  if (rows < columns) {
+    console.log("condition 1")
+    for (var c = 0; c < columns; c++) {
+      generateRow(rows, x_coor, y_coor);
+      y_coor += working_height * 2;
+    }
+  } else {
+    console.log("condition 2")
+    for (var r = 0; r < rows; r++) {
+      generateColumn(columns, x_coor, y_coor);
+      x_coor += working_width * 2;
+    }
   }
 }
 
 function generateRow(rows, x_coor, y_coor) {
   for (var r = 0; r < rows; r++) {
-    console.log("---" + y_coor);
+    console.log("generating row");
     ctx.fillStyle = "black";
     ctx.fillRect(x_coor, y_coor, working_width, working_height);
     x_coor += working_width * 2;
   }
 }
 
-generateDesks(rows, columns, x_coor, y_coor, working_width, working_height);
+function generateColumn(columns, x_coor, y_coor) {
+  for (var c = 0; c < columns; c++) {
+    console.log("generating column");
+    ctx.fillStyle = "black";
+    ctx.fillRect(x_coor, y_coor, working_width, working_height);
+    console.log(x_coor)
+    y_coor += working_height * 2;
+  }
+}
+
+generateDesks();
